89763488888ac1398ee9f0857ef5b522e3c35361
rushil joshi <you@example.com>
2022-01-15 17:57:47 -0500

    learned memory allocation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,14 @@ import java.util.Locale;
 public class Main { //public keyword is access modifier
 
     public static void main(String[] args) {
-        var textBox1 = new TextBox(); // we are creating a new instance of the textbox class
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text.toUpperCase());
+        var textBox1 = new TextBox(); // reference type variables store the address of the textbox object
+        // that is on the heap
+        var textBox2= textBox1; // textbox2 variable copies that value that textbox1 variable has
+        // therefore they are both pointing to the same object
 
-        var textBox2 = new TextBox(); // we are creating a new instance of the textbox class
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text.toUpperCase());
+        textBox2.setText("Hello World");
+        System.out.println(textBox1.text);
     }
 }
+
+// garbage collection automatically removes unused objects on the heap
